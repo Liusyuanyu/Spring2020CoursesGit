3fcef3dd93429e50a30f265dbfea688c4d1e3af5
HsuanYu <syuanyu@hotmail.com.tw>
2020-02-21 22:21:53 -0800

    AI A3_part 1 : Completed 1, 4. and 2, 3 unsure.

--- CS-550 Artificial Intelligence/Assignment 3/a3_Part1/newtonraphson.py
+++ CS-550 Artificial Intelligence/Assignment 3/a3_Part1/newtonraphson.py
@@ -11,6 +11,13 @@ def NewtonRaphson(fpoly, a, tolerance = .00001):
     This is an iterative method that stops when the 
     change in estimators is less than tolerance.    
 """ 
+    x0 = a
+    fpoly_prime = derivative(fpoly)
+    change = polyval(fpoly,x0)/polyval(fpoly_prime,x0) 
+    while(abs(change) > tolerance):
+        change = polyval(fpoly,x0)/polyval(fpoly_prime,x0) 
+        x0 = x0 - change
+    return x0
 
 
 def polyval(fpoly, x): 
@@ -22,7 +29,13 @@ def polyval(fpoly, x):
     Example:  f(x) = 4x^3 + 0x^2 + 9x^1 + 3 evaluated at x=5 
     polyval([4, 0, 9, 3], 5)) 
     returns 548 
-    """ 
+    """
+    power = len(fpoly)-1
+    value = 0
+    for num in fpoly:
+        value += num* x ** power
+        power -=1
+    return value 
  
 def derivative(fpoly): 
     """derivative(fpoly) 
@@ -35,3 +48,11 @@ def derivative(fpoly):
     derivative((3,4,5))  # 3 * x**2 + 4 * x**1 + 5 * x**0 
     returns:  [6, 4]     # 6 * x**1 + 4 * x**0 
     """ 
+    power = len(fpoly)-1
+    prime_fpoly = []
+    fpoly = fpoly[:-1]
+    for num in fpoly:
+        prime_fpoly.append(num*power)
+        power -=1
+    return prime_fpoly
+    
